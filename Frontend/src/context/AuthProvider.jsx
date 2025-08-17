ad2078b0c10902b22c0609fd5a4be8f26266bc4b
sudhanshu-m21 <sudhanshumishra106@gmail.com>
2025-08-17 10:41:46 +0530

    change file import config

--- Frontend/src/context/AuthProvider.jsx
+++ Frontend/src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useEffect, useState } from "react";
-import { getLocalStorage, setLocalStorage } from "../utils/localstorage";
+import { getLocalStorage, setLocalStorage } from "../utils/localStorage.jsx";
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
